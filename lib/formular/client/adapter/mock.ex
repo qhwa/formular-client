939001a50039ed748afcb7d709c780d206efd5b4
Qiu Hua <qiu@zubale.com>
2022-05-13 12:21:19 -0700

    chore: check mocked formulas

--- lib/formular/client/adapter/mock.ex
+++ lib/formular/client/adapter/mock.ex
@@ -1,8 +1,23 @@
+defmodule Formular.Client.MockError do
+  defexception [:message]
+
+  @impl true
+  def exception({:missing, _missing_formulas}) do
+    %Formular.Client.MockError{message: "Not all the formulas were mocked"}
+  end
+end
+
 defmodule Formular.Client.Adapter.Mock do
   alias Formular.Client.Cache
 
-  def start_link(_config, opts) do
+  require Logger
+
+  def start_link(config, opts) do
+    Logger.debug(["Starting mock formular intepreter."])
+
     {:ok, pid} = Agent.start_link(fn -> :ok end)
+
+    ensure_all_formulas_provided!(config.formulas, opts[:formulas])
 
     case opts[:formulas] do
       formulas when is_list(formulas) ->
@@ -15,6 +30,36 @@ defmodule Formular.Client.Adapter.Mock do
     end
 
     {:ok, pid}
+  end
+
+  defp ensure_all_formulas_provided!(required, provided) do
+    case ensure_all_formulas_provided(required, provided) do
+      :ok ->
+        :ok
+
+      {:missing, missing} ->
+        msg = """
+        Not all the formulas were mocked, missing:
+        #{inspect(missing)}.
+        """
+
+        Logger.error(msg)
+        # raise Formular.Client.MockError, {:missing, missing}
+        :ok
+    end
+  end
+
+  defp ensure_all_formulas_provided(required, provided) do
+    required = required |> Enum.map(&elem(&1, 1))
+    provided = provided |> Enum.map(&elem(&1, 0))
+
+    case required -- provided do
+      [] ->
+        :ok
+
+      missing ->
+        {:missing, missing}
+    end
   end
 
   def mock_global(name, function)
