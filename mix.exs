cd9950e8a1dddb02c5c54cc1197f99ce685558d3
Qiu Hua <qiu@zubale.com>
2022-05-13 17:02:46 -0700

    chore: support lower version of Elixir

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Formular.Client.MixProject do
       description:
         "Watch and fetch application's configuration from a server, and compile them into Elixir modules.",
       version: "0.2.0",
-      elixir: ">= 1.12.0",
+      elixir: ">= 1.10.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
