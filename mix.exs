191da6564b42957732edd106ce73ff2afdf27d50
Qiu Hua <qiu@zubale.com>
2023-03-02 06:02:22 -0800

    bump: v0.3.0-alpha.2

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Formular.Client.MixProject do
       app: :formular_client,
       description:
         "Watch and fetch application's configuration from a server, and compile them into Elixir modules.",
-      version: "0.3.0-alpha.1",
+      version: "0.3.0-alpha.2",
       elixir: ">= 1.10.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
